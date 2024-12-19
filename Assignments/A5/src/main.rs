a66fd6bc3cde92d4b1b4418a32822e01b11fccf8
Matthew Langton <60228542+ViableGolem4375@users.noreply.github.com>
2024-12-19 12:08:36 -0500

    Create main.rs

---
+++ Assignments/A5/src/main.rs
@@ -0,0 +1,14 @@
+mod memory;
+pub mod machine;
+pub mod bitpack;
+use std::env;
+
+/// Main function to run the program.
+/// 
+/// Arguments:
+/// 
+fn main() {
+    let input = env::args().nth(1);
+    let instructions: Vec<u32> = memory::load(input.as_deref());
+    memory::instructs(instructions.clone());
+}
